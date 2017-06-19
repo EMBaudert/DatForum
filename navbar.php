58d9d05ff8e7b3b052323d57341b8a3653e52956
Rino <rino@grupp.cc>
2017-06-19 11:14:32 +0200

    Header completed

--- navbar.php
+++ navbar.php
@@ -1,3 +1,8 @@
+<?php	
+ session_start();
+$_SESSION['logged'] = true;
+$_SESSION['PKID'] = 1;
+?>
 
 			<nav class="navbar navbar-inverted">
 				<div class="container-fluid">
@@ -12,20 +17,25 @@
 					<div class="collapse navbar-collapse" id="myNavbar">
 						<ul class="nav navbar-nav">
 							<li class="active"><a href="#">Home</a></li>
-							<li class="dropdown">
-								<a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">Page 1-1</a></li>
-									<li><a href="#">Page 1-2</a></li>
-									<li><a href="#">Page 1-3</a></li>
-								</ul>
-							</li>
-							<li><a href="#">Page 2</a></li>
-							<li><a href="#">Page 3</a></li>
-						</ul>
+                  </ul>
 						<ul class="nav navbar-nav navbar-right">
-							<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-							<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                  
+                     <?php
+                     if(isset($_SESSION['logged'])&&$_SESSION['logged']==true){
+                        echo "<li class=\"dropdown\">
+   								<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"intern.php?p=profile&uid=\"".$_SESSION['PKID'].
+                           "\"><span class=\"glyphicon glyphicon-user\"></span> Profile <span class=\"caret\"></span></a>
+   								<ul class=\"dropdown-menu\">
+   									<li><a href=\"#\">Profile</a></li>
+   									<li><a href=\"#\">Posts</a></li>
+   								</ul>
+   							</li>
+                        <li><a href=\"intern.php?p=logout\"><span class=\"glyphicon glyphicon-log-in\"> Logout</span> </a></li>";
+                     }else{
+   							echo "<li><a href=\"intern.php?p=register\"><span class=\"glyphicon glyphicon-user\"></span> Register</a></li>
+   							<li><a href=\"intern.php?p=login\"><span class=\"glyphicon glyphicon-log-in\"></span> Login</a></li>";
+                     }
+                     ?>
 						</ul>
 						<form class="navbar-form navbar-left">
 							<div class="input-group">
