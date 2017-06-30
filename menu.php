c7946f76b8c4317d0a1a36bc561151bd4a290a2b
Rino <rino@grupp.cc>
2017-06-30 18:49:24 +0200

    began post

--- menu.php
+++ menu.php
@@ -1,12 +1,8 @@
-
-   <?php
-      require_once 'func/menu.func.php';
-   ?>
+<?php
+   require_once 'func/menu.func.php';
+?>
 <!DOCTYPE html>
 <html>
-
-
-   
       <!-- Das neueste kompilierte und minimierte CSS -->
       <link rel="stylesheet" href="bootstrap/less/dist/css/bootstrap.min.css">
 
@@ -16,8 +12,6 @@
       <!-- Latest compiled and minified JavaScript -->
       <script src="bootstrap/jquery-3.2.1.min.js"></script>
       <script src="bootstrap/less/dist/js/bootstrap.min.js"></script>
-      
-      
       
    <head>
       <title>Forum</title>
